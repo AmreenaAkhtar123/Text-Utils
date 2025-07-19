efeaacc7109af95bf5fb35462f742e82d1786ec5
AmreenaAkhtar123 <amberkhannn7654@gmail.com>
2025-07-19 18:18:36 +0500

    Using the UseState Hook

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+  
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    //console.log("Uppercase was clicked" + text);
+  }
+
+  const handleOnChange = (event) => {
+    //console.log("On change");
+    setText(event.target.value);
+  } 
+  
+  const [text, setText] = useState("Enter your text here");
   return (
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
         
-        <textarea className="form-control " id="exampleFormControlTextarea1" rows="8"></textarea>
+        <textarea className="form-control " id="myBox" value={text} rows="8" onChange={handleOnChange}></textarea>
 
-        <button className="btn btn-primary my-3">Convert to UPPER CASE</button>
+        <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to UPPER CASE</button>
       </div>
     </div>
   )
